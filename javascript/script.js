15b283a024c2f161a9064b56d9e4020c4e0df54c
Charisse Strong <msstrong92@gmail.com>
2019-04-04 14:50:35 -0400

    almost done i need more obstacles

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,57 @@
 // https://www.youtube.com/channel/UCdS3ojA8RL8t1r18Gj1cl6w
+// https://www.youtube.com/watch?v=yq2au9EfeRQ&list=PLpPnRKq7eNW3We9VdCfx9fprhqXHwTPXL&index=4&t=614s
 // PothOnProgramming is responsible for my jumping animation
 
 
 let canvas = document.querySelector('canvas');
 let cxt = canvas.getContext('2d');
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-let rectangle = {
+
+let box = {
 	height: 50,
 	jumping: true,
 	width: 50,
-	x: 100,
+	x: 0,
 	x_velocity: 0,
 	y: 0,
 	y_velocity: 0
 };
 
+ function Obstacle (x, x_velocity, y){
+ 	this.x = x;
+	this.x_velocity = x_velocity;
+	this.y = y;
+
+  this.draw = ()=> {
+  	cxt.beginPath();
+	cxt.rect(this.x, this.y, 50, 100);
+	cxt.strokeStyle = 'magenta';
+	cxt.stroke();
+	cxt.fill();
+  }
+  this.update = ()=> {
+  	this.draw();
+  	if (this.x >= innerWidth) {
+		this.dx = 250;
+	}
+	this.x += this.dx;
+
+	
+  }
+}
+let  obstacleArray= [];
+for (let i = 0; i < 10; i++) {
+	let x = Math.random() * (innerWidth - 50);
+	let y = 700;
+	let dx = (Math.random()- 0.5) * 8;
+	obstacleArray.push(new Obstacle(x, y, dx));
+
+}
+
+
 let controller = {
-	left: false,
 	right: false,
 	up: false,
 	keyListener: function(event){
@@ -27,10 +60,7 @@ let controller = {
 
 		switch(event.keyCode){
 
-			case 37:
-				controller.left = key_state;
-			break;
-			case 38:
+			case 32:
 				controller.up = key_state;
 			break;
 			case 39:
@@ -42,131 +72,82 @@ let controller = {
 };
 
 let loop = function(){
-	if (controller.up && rectangle.jumping === false) {
-		rectangle.y_velocity -= 40;
-		rectangle.jumping = true;
-	}
-	if (controller.left) {
-		rectangle.x_velocity -= 0.5;
+	if (controller.up && box.jumping === false) {
+		box.y_velocity -= 40;
+		box.jumping = true;
 	}
 	if (controller.right) {
-		rectangle.x_velocity += 0.5
+		box.x_velocity += 0.5
 	}
 
-	rectangle.y_velocity += 1.5; //This simulates gravity meaning the rectangle will always fall.
-	rectangle.x += rectangle.x_velocity;
-	rectangle.y += rectangle.y_velocity;
-	rectangle.x_velocity *= 0.9; //Both lines simulate friction. The reducde the x and y velocity to make it look like you are gradually slowing down. 
-	rectangle.y_velocity *= 0.9;
+	box.y_velocity += 1.5; //This simulates gravity meaning the rectangle will always fall.
+	box.x += box.x_velocity;
+	box.y += box.y_velocity;
+	box.x_velocity *= 0.9; //Both lines simulate friction. The reducde the x and y velocity to make it look like you are gradually slowing down. 
+	box.y_velocity *= 0.9;
+	
 
-	 if(rectangle.y > 500){
-	 	rectangle.jumping = false;
-	 	rectangle.y = 500;
-	 	rectangle.y_velocity = 0;
+	 if(box.y > 750){
+	 	box.jumping = false;
+	 	box.y = 750;
+	 	box.y_velocity = 0;
 	}
 
-	if (rectangle.x < -200) {
-		rectangle.x = 500;
-	}
-	else if (rectangle.x > 500) {
-		rectangle.x = -200;
-	}
+// 	if(box.y >= obstacle.y && box.y <= obstacle.y+100){
+// 	if(box.x+50>=obstacle.x && box.x+50 <= obstacle.x+50){
+// 	box.x = obstacle.x-50;
+// 		// youLose();
+// 	}
+// }
+// 	if((box.x||box.x+50) >= obstacle.x && (box.x||box.x+50) <= obstacle.x+50){
+// 	if(box.y+50>=obstacle.y){
+// 		box.y = obstacle.y-50;
+// 		// youLose();
+// 	}
+// }
+
+	// cxt.clearRect(0,0, innerWidth, innerHeight);
+
+	for(let i = 0; i < obstacleArray.length; i++){
+		console.log(obstacleArray[i].update());
+
+	 } 
+
+	// obstacle.y_velocity += 1.5; //This simulates gravity meaning the rectangle will always fall.
+	// obstacle.x += obstacle.x_velocity;
+	// obstacle.y += obstacle.y_velocity;
+
+	//  if(obstacle.y > 700){
+	//  	obstacle.y = 700;
+	//  	obstacle.y_velocity = 0;
+	// }
 // This above is so if the rectangle goes of one side of the screen then "teleport it back to the right side and vice versa"
 
+	
 	cxt.fillStyle = '#202020';
-	cxt.fillRect(0,0,700,700);
+	cxt.fillRect(0,0,innerWidth,innerHeight);
 	cxt.fillStyle = 'magenta';
-	cxt.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+	cxt.rect(box.x, box.y, box.width, box.height);
 	cxt.fill();
 	cxt.strokeStyle = 'purple';
-	cxt.lineWidth = 4;
-	cxt.beginPath();
-	cxt.moveTo(500,500);
-	cxt.lineTo(500, 500);
-	cxt.stroke();
+
+
 
 	window.requestAnimationFrame(loop);
 
 };
 
+// function youLose() {
+// 	document.querySelector().addEventListener('', (){
+// 		document.querySelector('#bg-modal').style.display = 'flex';
+// 	})
+
+// 	document.querySelector('#close').addEventListener('click', (){
+// 		document.querySelector('#bg-modal').style.display = "none";
+// 	})
+// }
+
 
 window.addEventListener('keydown', controller.keyListener);
 window.addEventListener('keyup', controller.keyListener);
 window.requestAnimationFrame(loop);
-
-
-
-
-
-// cxt.fillStyle = 'rgba(255, 0, 0, 0.5)';
-// cxt.fillRect(100, 100, 100, 100);
-// cxt.fillStyle = 'rgba(0, 0, 255, 0.5)';
-// cxt.fillRect(300, 100, 100, 100);
-// cxt.fillStyle = 'rgba(0, 255, 0, 0.5)';
-// cxt.fillRect(500, 100, 100, 100);
-// // console.log(canvas);
-
-// // Drawing lines
-// // the use x and y values starting from the top left
-
-// cxt.beginPath();
-// cxt.moveTo(50, 300);
-// cxt.lineTo(300, 100);
-// cxt.lineTo(400, 100);
-// cxt.strokeStyle = '#fa34a3';
-// cxt.stroke();
-
-// function Circle (x, y, dx, dy, radius) {
-// 	this.x = x;
-// 	this.y = y;
-// 	this.dx = dx;
-// 	this.dy = dy;
-// 	this.radius = radius;
-
-//   this.draw = function(){
-//   	cxt.beginPath();
-// 	cxt.arc(this.x, this.y, this.radius,0, Math.PI * 2, false);
-// 	cxt.strokeStyle = 'magenta';
-// 	cxt.stroke();
-// 	cxt.fill();
-//   }
-//   this.update = function(){
-//   	if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-// 		this.dx = -this.dx;
-// 	}
-
-// 	if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-// 	this.dy = -this.dy;
-// }
-
-// 	this.x += this.dx;
-// 	this.y += this.dy;
-
-// 	this.draw();
-//   }
-// }
-
-
-// let circleArray = [];
-// for (let i = 0; i < 100; i++) {
-// 	let radius = 30;
-// 	let x = Math.random() * (innerWidth - radius * 2) + radius;
-// 	let y = Math.random() * (innerHeight- radius * 2) + radius;
-// 	let dx = (Math.random()- 0.5) * 8;
-// 	let dy = (Math.random() - 0.5) * 8;
-// 	circleArray.push(new Circle(x, y, dx, dy, radius));
-
-// }
-
-
-
-// let animate = ()=>{
-// 	requestAnimationFrame(animate);
-// 	cxt.clearRect(0,0, innerWidth, innerHeight);
-
-// 	for(let i = 0; i < circleArray.length; i++){
-// 		circleArray[i].update();
-// 	} 
-
-// }
-// animate();
